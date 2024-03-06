f764ff9716a040d0a7cccef26d1581f26eda0ca9
harry nguyen <nvhai245@gmail.com>
2024-03-06 12:08:00 +0700

    added reverse linked-list

---
+++ reverse-linked-list/reverse_linked_list.go
@@ -0,0 +1,17 @@
+package reverselinkedlist
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// recursive
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	newHead := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
+}
