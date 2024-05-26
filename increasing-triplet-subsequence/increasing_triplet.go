432f821ddc7f8e7979461baa613b98da53811d94
Hai Nguyen <nvhai245@gmail.com>
2024-05-26 17:16:54 +0700

    added increasing triplet subsequence

---
+++ increasing-triplet-subsequence/increasing_triplet.go
@@ -0,0 +1,17 @@
+package increasingtripletsubsequence
+
+import "math"
+
+func increasingTriplet(nums []int) bool {
+	minVal, minVal2 := math.Inf(0), math.Inf(0)
+	for _, num := range nums {
+		if float64(num) < minVal {
+			minVal = float64(num)
+		} else if float64(num) < minVal2 {
+			minVal2 = float64(num)
+		} else {
+			return true
+		}
+	}
+	return false
+}
